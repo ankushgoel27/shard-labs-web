dd6a6bf957d7dc170bb7c1b68d093412f14e5074
igor-lid <onaj.igor@gmail.com>
2020-03-05 16:48:11 +0100

    add overlay menu

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,6 +1,10 @@
 const menuButton = document.querySelector('.js-menu-button');
+const overlay = document.querySelector('.js-overlay');
 
 // Open and close menu
 menuButton.addEventListener('click', () => {
     menuButton.classList.toggle('is-active');
+    overlay.classList.toggle('overlay--is-active');
+
+    document.body.classList.toggle('no-scroll');
 })
