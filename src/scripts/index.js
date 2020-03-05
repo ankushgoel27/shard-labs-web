cbc90f4d269bac4ce143f3e3f99d3668ccb2ea4b
dark64 <darem966@gmail.com>
2020-03-05 20:02:41 +0100

    fix overlay, add media queries for mobile support

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,10 +1,22 @@
 const menuButton = document.querySelector('.js-menu-button');
 const overlay = document.querySelector('.js-overlay');
 
-// Open and close menu
-menuButton.addEventListener('click', () => {
+const toggleOverlay = () => {
     menuButton.classList.toggle('is-active');
-    overlay.classList.toggle('overlay--is-active');
-
+    overlay.classList.toggle('overlay--active');
     document.body.classList.toggle('no-scroll');
-})
+}
+
+// Open and close menu
+menuButton.addEventListener('click', toggleOverlay);
+
+document.querySelectorAll('a.overlay-navigation__link').forEach(e => {
+    e.addEventListener('click', toggleOverlay)
+});
+
+$(window).resize(() => {
+    let body = document.body;
+    if (body.clientWidth > 992 && overlay.classList.contains("overlay--active")) {
+        toggleOverlay();
+    }
+});
