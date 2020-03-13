a79cab833c5e391ccc6c7e71356569f0f20c17db
Jakov Buratovic <kraljipo007@gmail.com>
2020-03-13 20:22:54 +0100

    rerun

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,4 +6,4 @@ mkdir -p $REPO_PATH_TEST && \
 cd $REPO_PATH_TEST && \
 curl https://raw.githubusercontent.com/Shard-Labs/shard-labs-web/master/docker-compose.yaml > docker-compose.yaml && \
 docker-compose pull && \
-COMPOSE_PROJECT_NAME=shard-labs-web-stable docker-compose up -d"
+COMPOSE_PROJECT_NAME=shard-labs-web-stable docker-compose up -d" 
