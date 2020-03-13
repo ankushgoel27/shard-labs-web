9a851bde2f4aee6eb3259af7f708263247ec9d65
Jakov Buratovic <kraljipo007@gmail.com>
2020-03-13 20:14:05 +0100

    circle ci test flow

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,8 +2,8 @@
 set -e
 
 ssh -o "StrictHostKeyChecking no" user@ssh.shardlabs.io "\
-mkdir -p $REPO_PATH && \
-cd $REPO_PATH && \
+mkdir -p $REPO_PATH_TEST && \
+cd $REPO_PATH_TEST && \
 curl https://raw.githubusercontent.com/Shard-Labs/shard-labs-web/master/docker-compose.yaml > docker-compose.yaml && \
 docker-compose pull && \
 COMPOSE_PROJECT_NAME=shard-labs-web-stable docker-compose up -d"
