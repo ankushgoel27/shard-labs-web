d2b05048282b2f47ff02a2dd1856a1d16f28d10e
feyintoluwa <toluaina3@gmail.com>
2022-06-09 06:03:16 +0100

    refactored codes

--- scripts/publish_images.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-
-docker build --cache-from shardlabs/shard-labs-web:latest -t shardlabs/shard-labs-web:latest .
-
-docker login --username $DOCKER_USER --password $DOCKER_PASS
-docker push shardlabs/shard-labs-web:latest
